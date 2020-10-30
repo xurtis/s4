06b8c1f69ec5f9f64cc6c8feefab398d616f52e6
Curtis Millar <curtis@curtism.me>
2020-10-30 13:58:53 +1100

    Add global default config

--- src/config.rs
+++ src/config.rs
@@ -9,6 +9,9 @@ use toml;
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Deserialize)]
 /// Configuration for the whole s4 tool
 pub struct Config {
+    /// Global default configuration
+    #[serde(flatten)]
+    defaults: Defaults,
     /// Common flags
     #[serde(default, rename = "flag")]
     flags: BTreeSet<Flag>,
@@ -21,6 +24,67 @@ pub struct Config {
     /// Known projects
     #[serde(default, rename = "project")]
     projects: BTreeSet<Project>,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Deserialize)]
+#[serde(rename_all = "kebab-case")]
+pub struct Defaults {
+    /// Server to use for repo manifests
+    git_server: Option<String>,
+    /// Docker image for build tools
+    docker_image: Option<String>,
+    /// URL to download repo script
+    repo_url: Option<String>,
+    /// Git branch to check out with repo
+    repo_branch: Option<String>,
+    /// Repo manifest file to check out
+    repo_manifest: Option<String>,
+}
+
+impl Defaults {
+    /// Default git server for manifests
+    const GIT_SERVER: &'static str = "https://github.com";
+
+    /// Default docker image for build tools
+    const DOCKER_IMAGE: &'static str = "docker.io/trustworthysystems/camkes-riscv";
+
+    /// Default URL to download repo
+    const REPO_URL: &'static str = "https://storage.googleapis.com/git-repo-downloads/repo";
+
+    /// Get the git server base URL
+    pub fn git_server(&self) -> &str {
+        option_fallback(&self.git_server, Self::GIT_SERVER)
+    }
+
+    /// Docker image to execute for build tools
+    pub fn docker_image(&self) -> &str {
+        option_fallback(&self.docker_image, Self::DOCKER_IMAGE)
+    }
+
+    /// URL to download repo
+    pub fn repo_url(&self) -> &str {
+        option_fallback(&self.repo_url, Self::REPO_URL)
+    }
+
+    /// Branch to check out for repo
+    pub fn repo_branch(&self) -> Option<&str> {
+        option_ref(&self.repo_branch)
+    }
+
+    /// Manifest to checkou out for repo
+    pub fn repo_manifest(&self) -> Option<&str> {
+        option_ref(&self.repo_manifest)
+    }
+}
+
+/// Make reference option
+fn option_ref<T: AsRef<R>, R: ?Sized>(option: &Option<T>) -> Option<&R> {
+    option.as_ref().map(|s| s.as_ref())
+}
+
+/// Use a fallback reference if option is not set
+fn option_fallback<'t, T: AsRef<R>, R: ?Sized>(option: &'t Option<T>, fallback: &'t R) -> &'t R {
+    option_ref(option).unwrap_or(fallback)
 }
 
 impl Config {
